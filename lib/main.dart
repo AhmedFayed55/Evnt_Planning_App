993a9cae4f78f63faee5991b175578b479f56986
Fayed <anaa71137@gmail.com>
2025-02-14 12:44:25 -1000

    Add event ui and validation .

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:evnt_planning_app/providers/app_language_provider.dart';
 import 'package:evnt_planning_app/providers/app_theme_provider.dart';
 import 'package:evnt_planning_app/ui/auth/login/login_screen.dart';
 import 'package:evnt_planning_app/ui/auth/register/register_screen.dart';
+import 'package:evnt_planning_app/ui/home/tabs/home_tab/add_event/add_event.dart';
 import 'package:evnt_planning_app/ui/home/tabs/main_screen/main_screen.dart';
-import 'package:evnt_planning_app/ui/home/tabs/profile_tab/profile.dart';
 import 'package:evnt_planning_app/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -29,12 +29,12 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
-      initialRoute: LoginScreen.routeName,
+      initialRoute: MainScreen.routeName,
       routes: {
-        ProfileTab.routeName: (context) => ProfileTab(),
         MainScreen.routeName: (context) => MainScreen(),
         LoginScreen.routeName: (context) => LoginScreen(),
         RegisterScreen.routeName: (context) => RegisterScreen(),
+        AddEvent.routeName: (context) => AddEvent()
       },
       locale: Locale(languageProvider.appLanguage),
       theme: AppTheme.lightTheme,
